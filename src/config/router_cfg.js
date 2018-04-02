0cddef11409f5b74b7a966130dac6b704250101f
majing <jinggege@users.noreply.github.com>
2018-04-02 13:25:26 +0800

    create

---
+++ src/config/router_cfg.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        name:"big-public",
+        router:"/index",
+        controllerPath:"/controller/index.js",
+        methods:["GET","POST"],
+        desc:'首页'
+    }
+
+ 
+
+ 
+     
+   
+];
+
---
+++ src/config/server_cfg.js
@@ -0,0 +1,11 @@
+/**
+ * 服务器配置
+ */
+
+module.exports = {
+    server:{
+        port:4000
+    },
+    static_domian:'http://static.big.com'
+
+}
